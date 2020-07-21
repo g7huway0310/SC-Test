eb57847492baf10b2edfcb5fc4f3e49028753acc
lee <g7huway0310@gmail.com>
2020-07-21 21:54:16 +0800

    fianl

---
+++ SC Test/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  SC Test
+//
+//  Created by guowei on 2020/7/12.
+//  Copyright © 2020 guowei. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var Label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Label.text="Eadweard Muybridge (9 April 1830 – 8 May 1904, born Edward James Muggeridge) in Kingston upon Thames, was an English-American photographer important for his pioneering work in photographic studies of motion, and early work in motion-picture projection.He adopted the first name Eadweard as the original Anglo-Saxon form of Edward, and the surname Muybridge, believing it to be similarly archaic."
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SC Test/ViewController.swift
+++ SC Test/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate {
 
+    @IBOutlet weak var Scrollview: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addImage()
+        Scrollview.delegate=self
     }
 
 
+    func addImage(){
+        
+        var picture:[UIImage]=[]
+        
+        picture.append(UIImage(named: "The Horse in Motion")!)
+        picture.append(UIImage(named: "The Horse in Motion")!)
+        picture.append(UIImage(named: "The Horse in Motion")!)
+        
+        self.Scrollview.contentSize=CGSize(width: Scrollview.frame.size.width*CGFloat(picture.count), height: Scrollview.frame.size.height)
+        
+        for i in 0..<picture.count{
+            var frame=CGRect()
+            frame.origin.x=Scrollview.frame.size.width*CGFloat(i)
+            frame.origin.y=0
+            frame.size=Scrollview.frame.size
+            Scrollview.showsHorizontalScrollIndicator=false
+            let subView=UIImageView(frame:frame)
+            subView.image=picture[i]
+            subView.contentMode = .scaleAspectFill
+            self.Scrollview.addSubview(subView)
+            
+        }
+        
+    }
+    func scrollViewDidScroll(_ scrollView:UIScrollView){
+        let x=scrollView.contentOffset.x
+        if x>0{
+            if x>=scrollView.frame.size.width*2{
+                
+                self.Scrollview.contentOffset=CGPoint(x:scrollView.frame.size.width,y: 0)
+                
+            }
+        }
+        else if x<0{
+             self.Scrollview.contentOffset=CGPoint(x:scrollView.frame.size.width*1,y: 0)
+        }
+        
+        
+    }
+    
+    
+    
+    
 }
 
